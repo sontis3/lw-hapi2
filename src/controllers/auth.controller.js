4d1b58e868a5aeea0221494e849057ed709dd878
sontis3 <oleg.ovcharenko@labworks.ru>
2019-06-07 20:26:53 +0300

    +casl

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -4,6 +4,25 @@ const Boom = require('boom');
 const Dal = require('../models/dal/auth.dal');
 const Bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+// eslint-disable-next-line no-unused-vars
+const { AbilityBuilder, Ability } = require('@casl/ability');
+
+const CaslRules1 = [
+  {
+    actions: ['read'],
+    subject: ['all'],
+  },
+];
+const CaslRules2 = [
+  {
+    actions: ['read'],
+    subject: ['all'],
+  },
+  {
+    actions: ['read', 'delete'],
+    subject: ['Country'],
+  },
+];
 
 module.exports = {
   // Создать нового пользователя
@@ -27,6 +46,14 @@ module.exports = {
 
   // вход(login)
   async login(request, h) {
+    // eslint-disable-next-line no-unused-vars
+    const { rules, can, cannot } = AbilityBuilder.extract();
+    can('read', 'all');
+    can('manage', 'Post', { author: 'me' });
+    cannot('delete', 'Post');
+    // eslint-disable-next-line no-unused-vars
+    const aaa = new Ability(CaslRules1);
+
     const { name, password } = request.payload;
     let result = await Dal.findByName(name).catch(err => {
       return Boom.badRequest(err.message);
@@ -40,14 +67,14 @@ module.exports = {
       return Boom.badRequest('Пользователь деактивирован. Обратитесь к администратору системы.');
     }
 
-    const credential = { id: result.id };
+    const credential = { id: result.id, rules: CaslRules2 };
     // проверка пароля
     result = await Bcrypt.compare(password, result.password)
       .then(res => {
         if (res) {
           // const token = jwt.sign(credential, this.jwt_key, { algorithm: 'HS256', expiresIn: '1h' });
           const token = jwt.sign(credential, this.jwt_key, { algorithm: 'HS256' });
-          return { name: name, access_token: token };
+          return { name: name, access_token: token, rules: CaslRules2 };
         } else {
           return Boom.badRequest('Неверные логин или пароль.');
         }
